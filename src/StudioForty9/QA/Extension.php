a64fa6bb9ad334ebb63d219500eb4ddfc121e1c4
Alan Morkan <alan@studioforty9.com>
2015-03-02 23:16:07 +0000

    Fixing composer file.

---
+++ src/StudioForty9/QA/Extension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace StudioForty9\QA;
+
+use Behat\Behat\Extension\Extension as BaseExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\DependencyInjection\Loader\XmlFileLoader,
+    Symfony\Component\Config\FileLocator;
+
+class Extension extends BaseExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $container->setParameter('magento.base_url', \Mage::getStoreConfig('web/unsecure/base_url'));
+        //$container->setParameter('magento.behat_mage_dir',  \Mage::getConfig()->getModuleDir('', 'Hackathon_MageBehat') . DS . 'Test');
+        // $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/services'));
+        // $loader->load('core.xml');
+    }
+}
