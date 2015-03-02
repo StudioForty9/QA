84536ded3ffdc39898f10a76092b06f93e21a6e0
Alan Morkan <alan@studioforty9.com>
2015-03-02 23:20:43 +0000

    Getting base url to be automatically set for Behat.

--- src/StudioForty9/QA/Extension.php
+++ src/StudioForty9/QA/Extension.php
@@ -15,8 +15,5 @@ class Extension extends BaseExtension
     public function load(array $config, ContainerBuilder $container)
     {
         $container->setParameter('magento.base_url', \Mage::getStoreConfig('web/unsecure/base_url'));
-        //$container->setParameter('magento.behat_mage_dir',  \Mage::getConfig()->getModuleDir('', 'Hackathon_MageBehat') . DS . 'Test');
-        // $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/services'));
-        // $loader->load('core.xml');
     }
 }
